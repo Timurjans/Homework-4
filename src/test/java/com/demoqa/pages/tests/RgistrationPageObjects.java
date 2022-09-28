e1ff0ec5b22e4ebc877fe99880266ad41db6fe4a
Хазиев Тимур Дамирович <Haziev-TD@rshbintech.ru>
2022-09-28 15:48:03 +0300

    first commit

--- src/test/java/com/demoqa/pages/tests/RgistrationPageObjects.java
+++ src/test/java/com/demoqa/pages/tests/RgistrationPageObjects.java
@@ -13,6 +13,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+
 public class REGISTRATIONpageobjects {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
 
